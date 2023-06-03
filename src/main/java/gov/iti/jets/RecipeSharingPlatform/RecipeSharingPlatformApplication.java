c7a498aa0f70c2fe175f501925c0f31b61ea6b23
MarowaAdel28 <marwaadelmustafa28@gmail.com>
2023-06-03 23:23:27 +0300

    initial commit

---
+++ src/main/java/gov/iti/jets/RecipeSharingPlatform/RecipeSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package gov.iti.jets.RecipeSharingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSharingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/iti/jets/RecipeSharingPlatform/RecipeSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.iti.jets.RecipeSharingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
