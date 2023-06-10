3c1a65cc29eb3366dec7a35fe4806614480420b1
Kareem Alaa <kareemalaa473@gmail.com>
2023-06-10 20:39:54 +0300

    Front-end setup v2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './client/user/user.component';
 
 @NgModule({
   declarations: [
