e19fbb1471371edbec6ca85eac528ab3181632b1
Dina Mishahed <53543119+Dina-Mishahed@users.noreply.github.com>
2023-06-17 15:58:51 +0300

    add all recipes path

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import {HomeComponent} from "./components/home/home.component";
 
 import {RecipesComponent} from './components/recipes/recipes.component';
 import {DishAreaComponent} from "./components/dish-area/dish-area.component";
+import {AllRecipesComponent} from "./components/all-recipes/all-recipes.component";
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'view-recipe' , component:ViewRecipeComponent},
   {path:'recipes' , component:RecipesComponent},
-  {path:'dish_area' , component:DishAreaComponent}
-
+  {path:'dish_area' , component:DishAreaComponent},
+  {path:'all-recipes' , component:AllRecipesComponent}
 ];
 
 @NgModule({
